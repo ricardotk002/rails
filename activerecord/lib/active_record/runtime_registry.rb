c37a81c1113735fd129fc484701e1af9c0fa430e
Ricardo Díaz <ricardotk002@gmail.com>
2019-06-10 20:34:56 -0500

    Deprecate AS::PerThreadRegistry in ActiveRecord::RuntimeRegistry

--- activerecord/lib/active_record/runtime_registry.rb
+++ activerecord/lib/active_record/runtime_registry.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "active_support/per_thread_registry"
+require "active_support/core_ext/module/attribute_accessors_per_thread"
 
 module ActiveRecord
   # This is a thread locals registry for Active Record. For example:
@@ -9,16 +9,10 @@ module ActiveRecord
   #
   # returns the connection handler local to the current thread.
   #
-  # See the documentation of ActiveSupport::PerThreadRegistry
+  # See the documentation of Module::thread_mattr_accessor
   # for further details.
   class RuntimeRegistry # :nodoc:
-    extend ActiveSupport::PerThreadRegistry
-
-    attr_accessor :connection_handler, :sql_runtime
-
-    [:connection_handler, :sql_runtime].each do |val|
-      class_eval %{ def self.#{val}; instance.#{val}; end }, __FILE__, __LINE__
-      class_eval %{ def self.#{val}=(x); instance.#{val}=x; end }, __FILE__, __LINE__
-    end
+    thread_mattr_accessor :connection_handler
+    thread_mattr_accessor :sql_runtime
   end
 end
