68c91a7fbad6cc6c42249bc08415105d4d76c14e
Ricardo Díaz <ricardotk002@gmail.com>
2019-06-10 20:34:16 -0500

    Deprecate AS::PerThreadRegistry in ActiveRecord::ExplainRegistry

--- activerecord/lib/active_record/explain_registry.rb
+++ activerecord/lib/active_record/explain_registry.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require "active_support/per_thread_registry"
+require "active_support/core_ext/module/attribute_accessors_per_thread"
 
 module ActiveRecord
   # This is a thread locals registry for EXPLAIN. For example
@@ -9,24 +9,21 @@ module ActiveRecord
   #
   # returns the collected queries local to the current thread.
   #
-  # See the documentation of ActiveSupport::PerThreadRegistry
+  # See the documentation of Module::thread_mattr_accessor
   # for further details.
   class ExplainRegistry # :nodoc:
-    extend ActiveSupport::PerThreadRegistry
+    thread_mattr_accessor :queries, default: []
+    thread_mattr_accessor :collect, default: false
 
-    attr_accessor :queries, :collect
+    class << self
+      def collect?
+        collect
+      end
 
-    def initialize
-      reset
-    end
-
-    def collect?
-      @collect
-    end
-
-    def reset
-      @collect = false
-      @queries = []
+      def reset
+        self.queries = []
+        self.collect = false
+      end
     end
   end
 end
